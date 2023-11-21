5ef21e251438f35fcf2f9b72eaf5719ae03b5590
jpsehyun <jpsehyun@gmail.com>
2023-11-21 14:14:55 +0800

    input check

--- app/src/main/java/com/SEHYUN_PARK/csadprg_mp/Salary6Activity.kt
+++ app/src/main/java/com/SEHYUN_PARK/csadprg_mp/Salary6Activity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Salary6Activity : AppCompatActivity() {
@@ -56,77 +57,90 @@ class Salary6Activity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        computeBtn.setOnClickListener{
+        computeBtn.setOnClickListener {
             selectedDay = spinner.selectedItem.toString()
-            startTime = startEtv.getText().toString().toInt()
-            endTime = endEtv.getText().toString().toInt()
+            var correctInput = true
 
-            var ctr: Int = startTime
-            var hours_Worked: Int = 0
-            var overTime: Int = 0
-            var night_Overtime : Int = 0
-            var night_Shift: Int = 0
-
-            if (endTime == 0)
-                endTime = 2400
-
-            if (startTime > endTime){
-                endTime += 2400
+            try {
+                startTime = startEtv.getText().toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Please input correct time", Toast.LENGTH_SHORT).show()
+                correctInput = false
             }
 
-            for (i in startTime .. endTime step 100){
+            try {
+                endTime = endEtv.getText().toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Please input correct time", Toast.LENGTH_SHORT).show()
+                correctInput = false
+            }
 
-                if (ctr == 2300 || ctr == 2400 || ctr <= 600){
-                    if (hours_Worked > Computation.getNumWorkHour() + 1){
-                        night_Overtime++
+            if (correctInput) {
+                var ctr: Int = startTime
+                var hours_Worked: Int = 0
+                var overTime: Int = 0
+                var night_Overtime: Int = 0
+                var night_Shift: Int = 0
+
+                if (endTime == 0)
+                    endTime = 2400
+
+                if (startTime > endTime) {
+                    endTime += 2400
+                }
+
+                for (i in startTime..endTime step 100) {
+
+                    if (ctr == 2300 || ctr == 2400 || ctr <= 600) {
+                        if (hours_Worked > Computation.getNumWorkHour() + 1) {
+                            night_Overtime++
+                        } else {
+                            night_Shift++
+                        }
                     }
-                    else{
-                        night_Shift++
+
+                    if (hours_Worked > Computation.getNumWorkHour() + 1) {
+                        if (!(ctr == 2300 || ctr == 2400 || ctr <= 600)) {
+                            overTime++
+                        }
+
+                    }
+
+                    hours_Worked += 1
+                    ctr += 100
+                    if (i == 2400)
+                        ctr = 0;
+                }
+
+                hours_Worked -= 1
+                if (startTime == 2300 || startTime == 2400 || startTime == 0 || startTime <= 600) {
+                    night_Shift -= 1
+                }
+
+                var dailyRate: Float = Computation.getDailySalary() / Computation.getNumWorkHour()
+
+                var computation = Computation(selectedDay)
+                var OT = dailyRate * overTime * computation.overTime
+                var NSOT = dailyRate * night_Overtime * computation.nightOverTime
+                var NS: Float = dailyRate * night_Shift * 1.1f
+                var baseSalary = Computation.getDailySalary() * computation.dailyRate
+                var totalSalary = baseSalary + NS + NSOT + OT
+
+                if (startTime == 900 && endTime == 900) {
+                    if (selectedDay.equals("Rest Day")) {
+                        totalSalary = Computation.getDailySalary()
+                    } else {
+                        totalSalary = 0f
                     }
                 }
 
-                if (hours_Worked > Computation.getNumWorkHour() + 1){
-                    if (!(ctr == 2300 || ctr == 2400 || ctr <= 600)){
-                        overTime++
-                    }
+                overtimeTv.setText(overTime.toString() + "(" + night_Overtime.toString() + ")")
+                nightShift.setText(night_Shift.toString())
+                salaryTotal.setText((totalSalary).toString())
 
-                }
+                DailySalary.setSixth((totalSalary).toString())
 
-                hours_Worked += 1
-                ctr += 100
-                if (i == 2400)
-                    ctr = 0;
             }
-
-            hours_Worked -= 1
-            if (startTime == 2300 || startTime == 2400 || startTime == 0 || startTime <= 600){
-                night_Shift -= 1
-            }
-
-            var dailyRate: Float = Computation.getDailySalary() / Computation.getNumWorkHour()
-
-            var computation =  Computation(selectedDay)
-            var OT = dailyRate * overTime * computation.overTime
-            var NSOT = dailyRate * night_Overtime * computation.nightOverTime
-            var NS: Float = dailyRate * night_Shift * 1.1f
-            var baseSalary = Computation.getDailySalary() * computation.dailyRate
-            var totalSalary = baseSalary+NS+NSOT+OT
-
-            if (startTime == 900 && endTime == 900){
-                if (selectedDay.equals("Rest Day")){
-                    totalSalary = Computation.getDailySalary()
-                }
-                else{
-                    totalSalary = 0f
-                }
-            }
-
-            overtimeTv.setText(overTime.toString() + "(" + night_Overtime.toString() + ")")
-            nightShift.setText(night_Shift.toString())
-            salaryTotal.setText((totalSalary).toString())
-
-            DailySalary.setSixth((totalSalary).toString())
-
         }
 
         nextBtn.setOnClickListener{
--- app/src/main/java/com/SEHYUN_PARK/csadprg_mp/SalaryActivity.kt
+++ app/src/main/java/com/SEHYUN_PARK/csadprg_mp/SalaryActivity.kt
@@ -2,11 +2,13 @@ package com.SEHYUN_PARK.csadprg_mp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SalaryActivity : AppCompatActivity() {
@@ -53,77 +55,90 @@ class SalaryActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-        computeBtn.setOnClickListener{
+        computeBtn.setOnClickListener {
             selectedDay = spinner.selectedItem.toString()
-            startTime = startEtv.getText().toString().toInt()
-            endTime = endEtv.getText().toString().toInt()
+            var correctInput = true
 
-            var ctr: Int = startTime
-            var hours_Worked: Int = 0
-            var overTime: Int = 0
-            var night_Overtime : Int = 0
-            var night_Shift: Int = 0
+            try {
+                startTime = startEtv.getText().toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Please input correct time", Toast.LENGTH_SHORT).show()
+                correctInput = false
+            }
 
-            if (endTime == 0)
+            try {
+                endTime = endEtv.getText().toString().toInt()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Please input correct time", Toast.LENGTH_SHORT).show()
+                correctInput = false
+            }
+
+            if (correctInput) {
+                var ctr: Int = startTime
+                var hours_Worked: Int = 0
+                var overTime: Int = 0
+                var night_Overtime: Int = 0
+                var night_Shift: Int = 0
+
+                if (endTime == 0)
                     endTime = 2400
 
-            if (startTime > endTime){
-                endTime += 2400
-            }
-
-            for (i in startTime .. endTime step 100){
-
-                if (ctr == 2300 || ctr == 2400 || ctr <= 600){
-                    if (hours_Worked > Computation.getNumWorkHour() + 1){
-                        night_Overtime++
-                    }
-                    else{
-                        night_Shift++
-                    }
+                if (startTime > endTime) {
+                    endTime += 2400
                 }
 
-                if (hours_Worked > Computation.getNumWorkHour() + 1){
-                    if (!(ctr == 2300 || ctr == 2400 || ctr <= 600)){
-                        overTime++
+                for (i in startTime..endTime step 100) {
+
+                    if (ctr == 2300 || ctr == 2400 || ctr <= 600) {
+                        if (hours_Worked > Computation.getNumWorkHour() + 1) {
+                            night_Overtime++
+                        } else {
+                            night_Shift++
+                        }
                     }
 
+                    if (hours_Worked > Computation.getNumWorkHour() + 1) {
+                        if (!(ctr == 2300 || ctr == 2400 || ctr <= 600)) {
+                            overTime++
+                        }
+
+                    }
+
+                    hours_Worked += 1
+                    ctr += 100
+                    if (i == 2400)
+                        ctr = 0;
                 }
 
-                hours_Worked += 1
-                ctr += 100
-                if (i == 2400)
-                    ctr = 0;
-            }
-
-            hours_Worked -= 1
-            if (startTime == 2300 || startTime == 2400 || startTime == 0 || startTime <= 600){
-                night_Shift -= 1
-            }
-
-            var dailyRate: Float = Computation.getDailySalary() / Computation.getNumWorkHour()
-
-            var computation =  Computation(selectedDay)
-            var OT = dailyRate * overTime * computation.overTime
-            var NSOT = dailyRate * night_Overtime * computation.nightOverTime
-            var NS: Float = dailyRate * night_Shift * 1.1f
-            var baseSalary = Computation.getDailySalary() * computation.dailyRate
-            var totalSalary = baseSalary+NS+NSOT+OT
-
-            if (startTime == 900 && endTime == 900){
-                if (selectedDay.equals("Rest Day")){
-                    totalSalary = Computation.getDailySalary()
+                hours_Worked -= 1
+                if (startTime == 2300 || startTime == 2400 || startTime == 0 || startTime <= 600) {
+                    night_Shift -= 1
                 }
-                else{
-                    totalSalary = 0f
+
+                var dailyRate: Float = Computation.getDailySalary() / Computation.getNumWorkHour()
+
+                var computation = Computation(selectedDay)
+                var OT = dailyRate * overTime * computation.overTime
+                var NSOT = dailyRate * night_Overtime * computation.nightOverTime
+                var NS: Float = dailyRate * night_Shift * 1.1f
+                var baseSalary = Computation.getDailySalary() * computation.dailyRate
+                var totalSalary = baseSalary + NS + NSOT + OT
+
+                if (startTime == 900 && endTime == 900) {
+                    if (selectedDay.equals("Rest Day")) {
+                        totalSalary = Computation.getDailySalary()
+                    } else {
+                        totalSalary = 0f
+                    }
                 }
+
+                overtimeTv.setText(overTime.toString() + "(" + night_Overtime.toString() + ")")
+                nightShift.setText(night_Shift.toString())
+                salaryTotal.setText((totalSalary).toString())
+
+                DailySalary.setFirst((totalSalary).toString())
+
             }
-
-            overtimeTv.setText(overTime.toString() + "(" + night_Overtime.toString() + ")")
-            nightShift.setText(night_Shift.toString())
-            salaryTotal.setText((totalSalary).toString())
-
-            DailySalary.setFirst((totalSalary).toString())
-
         }
 
         nextBtn.setOnClickListener{
